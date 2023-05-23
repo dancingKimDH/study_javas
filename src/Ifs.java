1bbfdf64c8690a4abf8f7c9ea33cd9155daaf1d0
dancingkimDH <kimdonghyun0701@gmail.com>
2023-05-23 17:23:35 +0900

    ifs

---
+++ src/Ifs.java
@@ -0,0 +1,16 @@
+public class Ifs {
+    public static void main(String[] args) {
+        // if () {
+        // // block of code to be executed if the condition is true
+        // }
+        int first = 20;
+        int second = 18;
+        
+        if (first > second) {
+            System.out.println(first);
+        }
+        System.out.println("Hello, World!");
+        // return 0;
+    }
+
+}
