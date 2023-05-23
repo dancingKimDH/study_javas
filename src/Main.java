56047e023c3ee75c719de00e5ecf71337a5f7a45
dancingkimDH <kimdonghyun0701@gmail.com>
2023-05-23 16:09:55 +0900

    java practice

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ public class Main {
     public static void main(String[] args) {
         String name = "Donghyun Kim" ;
         System.out.println(name);
+        name = "Kim";
+        System.out.println();
+
         // return 0;
     }
 }
---
+++ src/quest.java
@@ -0,0 +1,18 @@
+import javax.swing.Spring;
+
+public class quest {
+    public static void main(String[] args) {
+
+        int N1 = 1;
+        int N2 = 2;
+        int N3 = 3;
+
+        N3 = N1;
+        N1 = N2;
+
+        System.out.println(N3);
+        System.out.println(N1);
+
+    }
+
+}
