b70b4efe7ff45cbce2e94c0fab753314fda74a54
dancingkimDH <kimdonghyun0701@gmail.com>
2023-05-24 15:21:15 +0900

    loopwithmark

---
+++ src/LoopWithMark.java
@@ -0,0 +1,10 @@
+public class LoopWithMark {
+    public static void main(String[] args) {
+        for(int line = 1; line < 5; line = line + 1) {
+            System.out.println("for " + line);
+        }
+
+
+        // return 0;
+    }
+}
