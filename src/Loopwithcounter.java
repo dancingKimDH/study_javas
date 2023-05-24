8abb7b418db90623e8f41f8b3d568d667f647b9c
dancingkimDH <kimdonghyun0701@gmail.com>
2023-05-24 10:56:40 +0900

    practice

---
+++ src/Loopwithcounter.java
@@ -0,0 +1,14 @@
+public class Loopwithcounter {
+    public static void main(String[] args) {
+        int A = 5;
+        int B = 1;
+        int C = 1;
+
+        while (C <= A) {
+            System.out.println(B);
+            C = C + 1;
+            B = B + 1;
+        }
+
+    }
+}
