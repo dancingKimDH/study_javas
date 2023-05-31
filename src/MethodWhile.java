f7ed8bbc8196af68421039eeefaed41d456fee5a
dancingkimDH <kimdonghyun0701@gmail.com>
2023-05-31 10:10:21 +0900

    update

--- src/MethodWhile.java
+++ src/MethodWhile.java
@@ -1,21 +1,30 @@
 import java.util.Scanner;
+
 public class MethodWhile {
     public String gettime(int time) {
         String str = "";
-        while (time <= 12) {
-            str = "오전입니다.";
-            break;
+        try {
+            while (time <= 12) {
+                str = "오전입니다.";
+                break;
+            }
+            while (time > 12 && time <= 18) {
+                str = "오후입니다.";
+                break;
+            }
+            while (time > 18) {
+                str = "밤입니다.";
+                break;
+            }
+        } catch (Exception e) {
         }
-        while (time > 12 && time <= 18) {
-            str = "오후입니다.";
-            break;
-        }
-        while (time > 18) {
-            str = "밤입니다.";
-            break;
-        }
+
         return str;
     }
+
+
+
+
     public static void main(String[] args) {
         Scanner myObj = new Scanner(System.in);
         int time = myObj.nextInt();
