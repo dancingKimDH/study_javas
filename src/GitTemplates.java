fee11ef296dfdb9213a22a1a73ec98adbc14c358
dancingkimDH <kimdonghyun0701@gmail.com>
2023-05-31 11:21:42 +0900

    modify

--- src/GitTemplates.java
+++ src/GitTemplates.java
@@ -1,14 +1,5 @@
 public class GitTemplates {
-    public static void main(String[] args) {
-        try {
-            int first = 0;
-            int second = 1;
-            second = first + 1;
-            
-        } catch (Exception e) {
-            // TODO: handle exception
-        } //return 0;
-    }
+
    public static void main(String[] args) {
     try {
         int first = 0;
