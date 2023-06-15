0d99cda13e940001554a6bd3eba8457e0c5f1b84
dancingkimDH <kimdonghyun0701@gmail.com>
2023-06-15 16:10:33 +0900

    update

--- src/SurveyApp.java
+++ src/SurveyApp.java
@@ -4,9 +4,21 @@ import java.sql.*;
 
 public class SurveyApp {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
         try {
+            String url = "jdbc:mysql://localhost:3306/db_survey";
+            String user = "root";
+            String password = "!yojulab*";
+            Connection connection = DriverManager.getConnection(url, user, password);
+            System.out.println("DB연결 성공\n");
 
+            Statement statement = connection.createStatement();
+            String query = "SELECT * FROM db_cars.factorys";
+            ResultSet resultSet = statement.executeQuery(query);
+            while (resultSet.next()) {
+                resultSet.getString("COMPANY_ID");
+                resultSet.getString("COMPANY");
+            }
         } catch (Exception e) {
             System.out.println(e.getMessage());
             // TODO: handle exception
