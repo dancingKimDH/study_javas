5e16c1a3d00ecc68fef4cdb6b85565263f2c9d89
ARAJO <dddd2511u@gmail.com>
2023-06-15 16:57:22 +0900

    first commit

--- src/SurveyApp.java
+++ src/SurveyApp.java
@@ -8,10 +8,20 @@ public class SurveyApp {
             String password = "!yojulab*";
             Connection connection = DriverManager.getConnection(url, user, password);
             System.out.println("DB연결 성공\n");
-
+            
+            //- query Edit
             Statement statement = connection.createStatement();
-            String query = "SELECT * FROM db_cars.factorys";
-            ResultSet resultSet = statement.executeQuery(query);
+            System.out.println("--- 통계 ---");
+
+            
+            // -- 총 설문자 : 3명
+            String queryB = "SELECT COUNT(*) CNT\n" + //
+                    "FROM (\n" + //
+                    "SELECT RESPONDENTS_ID, COUNT(*) CNT\n" + //
+                    "FROM statistics\n" + //
+                    "GROUP BY RESPONDENTS_ID\n" + //
+                    ") AS T_STATIC\n";
+            ResultSet resultSet = statement.executeQuery(queryB);
             while (resultSet.next()) {
                 resultSet.getString("COMPANY_ID");
                 resultSet.getString("COMPANY");
