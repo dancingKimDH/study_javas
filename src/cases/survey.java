c87ca45bf7fe3d47afbf044df67733762b86ad3c
dancingkimDH <kimdonghyun0701@gmail.com>
2023-06-15 15:10:13 +0900

    update

---
+++ src/cases/survey.java
@@ -0,0 +1,13 @@
+package cases;
+
+public class survey {
+    public static void main(String[] args) {
+
+        try {
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            // TODO: handle exception
+        }
+    }
+}
