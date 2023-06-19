1c5fba48b7a61129633c50de72603749ba1ede54
dancingkimDH <kimdonghyun0701@gmail.com>
2023-06-19 16:22:38 +0900

    update

--- src/Inheritances.java
+++ src/Inheritances.java
@@ -1,26 +1,35 @@
 // import inheritances.ObjectInheritances;
 // import vehicles.MountainBike;
 // import vehicles.bicycle;
-import vehicles.RoadCycle;
+// import vehicles.RoadCycle;
+import vehicles.Car;
+
 
 public class Inheritances {
     public static void main(String[] args) {
         try {
-            //OBJECT 확인용
+            // OBJECT 확인용
             // ObjectInheritances objectInheritances = new ObjectInheritances();
             
-            //this. 확인용
+            // this. 확인용
             // bicycle Bicycle = new bicycle(5500, "blue");
             // System.out.println(Bicycle.toString());
             
-            //상속 확인용
+            // 상속 확인용
             // MountainBike mountainBike = new MountainBike();
             
-            //super.
+            // super.
             // MountainBike mountainBike = new MountainBike(6200, "yellow", 20);
             
-            RoadCycle roadcycle = new RoadCycle(200000, "Blue", "Super-Fast Bike");
-            System.out.println(roadcycle.toString());
+            // 상속 연습
+            // RoadCycle roadcycle = new RoadCycle(200000, "Blue", "Super-Fast Bike");
+            // System.out.println(roadcycle.toString());
+
+            // 구현 호출
+            Car car = new Car();
+            car.speedUp(1);
+            car.speedDown(2);
+            System.out.println();
 
             System.out.println();
 
--- src/vehicles/Vehicle.java
+++ src/vehicles/Vehicle.java
@@ -3,4 +3,5 @@ package vehicles;
 public interface Vehicle {
     public int speedUp(int val);
     public int speedDown(int val);
+    
 }
