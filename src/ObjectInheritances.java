5ed287142d90c2af8c92447860d0eb5f158f318d
dancingkimDH <kimdonghyun0701@gmail.com>
2023-06-19 11:11:50 +0900

    update

---
+++ src/ObjectInheritances.java
@@ -0,0 +1,11 @@
+public class ObjectInheritances {
+    public static void main(String[] args) {
+        try {
+            
+            
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            // TODO: handle exception
+        }
+    }
+}
